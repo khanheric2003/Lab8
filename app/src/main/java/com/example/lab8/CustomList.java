cb59d31610d96b0df334f0b2142e0c0b89e421ef
khanheric <duykhanh.eric@gmail.com>
2023-03-08 11:00:28 -0700

    RED: not implemented hasCity

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -76,17 +76,17 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
 
-//    /**
-//     *  This function check whether city belong in the List
-//     * @param city
-//     * @return
-//     *      return boolean whether the city inside cityList
-//     */
-//    public boolean hasCity(City city) {
-//        return false;
-////        if(cities.contains(city)) {return true;}
-////        else {return false;}
-//    }
+    /**
+     *  This function check whether city belong in the List
+     * @param city
+     * @return
+     *      return boolean whether the city inside cityList
+     */
+    public boolean hasCity(City city) {
+        return false;
+//        if(cities.contains(city)) {return true;}
+//        else {return false;}
+    }
 
 
 }
